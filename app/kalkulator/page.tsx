9b96ddeab0b43c9cd0820dae01e82e9948e8be72
yehezkiellrio <yehezkielrio17@gmail.com>
2026-07-21 20:10:21 +0700

    del

--- app/kalkulator/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import Calculator from '@/components/Calculator';
-import { useCatalog } from '@/components/CatalogProvider';
-
-export default function KalkulatorPage() {
-  const { products } = useCatalog();
-  return <div className="shell page-stack"><div className="page-title"><h1>Kalkulator</h1></div><Calculator products={products}/></div>;
-}
